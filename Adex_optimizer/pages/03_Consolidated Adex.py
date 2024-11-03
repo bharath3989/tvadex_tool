b3f02f1728977ad7220be3fc156ea72a48b3cd93
bharath3989 <97175566+bharath3989@users.noreply.github.com>
2024-11-03 16:55:53 +0530

    Update 03_Consolidated Adex.py

--- Adex_optimizer/pages/03_Consolidated Adex.py
+++ Adex_optimizer/pages/03_Consolidated Adex.py
@@ -8,7 +8,7 @@ import openpyxl
 st.set_page_config(page_title="Consolidated Adex",
                    page_icon="image.png", layout="wide")
 
-with open("/Users/bharathviswanathan/Desktop/Adex_optimizer/style.css") as f:
+with open("Adex_optimizer/style.css") as f:
     st.markdown(f"<style> {f.read()} </style>", unsafe_allow_html=True)
 
 st.markdown("# Consolidated Adex",
